a00e534487dcee99ef4a73268395bf6fe7cfdfdc
Joe Flynn <jflyn@gmail.com>
2025-07-19 18:07:37 +0100

    Fix: use pg.Pool for Vercel db connection

--- src/lib/db/pg/db.pg.ts
+++ src/lib/db/pg/db.pg.ts
@@ -1,3 +1,8 @@
-import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const pgDb = drizzlePg(process.env.POSTGRES_URL!);
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export const pgDb = drizzle(pool);
